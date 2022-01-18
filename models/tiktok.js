9e8d37a1473e5d51b740a49e722982d6aa39bbd8
Naufal Miswadi <naufal.miswadi@gmail.com>
2022-01-18 15:24:00 +0800

    created schema for tiktok posts

---
+++ models/tiktok.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tiktokSchema = new Schema({
+    publishAt: {
+        type: Date,
+        required: true
+    },
+    imageURL: {
+        type: String,
+        required: true
+    }
+},
+    { timestamps: true }
+);
+
+
+
+module.exports = mongoose.model('ttPost', tiktokSchema);
