a2c94ec3b1f3721260c7b9d274e2e16a4545bbb3
Naufal Miswadi <naufal.miswadi@gmail.com>
2022-01-20 18:37:48 +0800

    migrated more middleware to this dedicated file

--- middleware.js
+++ middleware.js
@@ -1,8 +1,33 @@
+const igPost = require('./models/instagram');
+const { smSchema } = require('./joiSchemas');
+const ExpressError = require('./utilities/ExpressError');
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
         req.flash('error', 'You are not signed in');
-        res.redirect('/login');
+        return res.redirect('/login');
     }
     next()
+}
+
+module.exports.validatePosts = (req, res, next) => {
+    const { error } = smSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const post = await igPost.findById(id);
+    if (!post.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect('/instagram');
+    }
+    next();
 }
