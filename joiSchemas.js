519deeb305aa783203552b39f3e537dc5bce29ca
Naufal Miswadi <naufal.miswadi@gmail.com>
2022-01-25 23:45:34 +0800

    commented out validation for new image upload method

--- joiSchemas.js
+++ joiSchemas.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 module.exports.smSchema = Joi.object({
     post: Joi.object({
         publishAt: Joi.date().required(),
-        imageURL: Joi.string().required(),
+        // imageURL: Joi.string().required(),
         caption: Joi.string().required()
     }).required()
 });
