d893c8c9f0c0203b5fc209c26c5fd0f00c4fff00
Naufal Miswadi <naufal.miswadi@gmail.com>
2022-02-15 11:18:36 +0800

    updated db path

--- agenda/agenda.js
+++ agenda/agenda.js
@@ -2,7 +2,7 @@ const Agenda = require("agenda");
 const User = require('../models/user');
 const fetch = require('node-fetch');
 
-const dbUrl = 'mongodb://localhost:27017/sm-scheduler';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/sm-scheduler';
 
 const agenda = new Agenda({
     db: { address: dbUrl, collection: "agendaJobs" },
