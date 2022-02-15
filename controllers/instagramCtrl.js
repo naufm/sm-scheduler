44ea136ad7d3aa79d3fe113313a152014a619cc2
Naufal Miswadi <naufal.miswadi@gmail.com>
2022-02-15 14:55:52 +0800

    fixed syntax error

--- controllers/instagramCtrl.js
+++ controllers/instagramCtrl.js
@@ -104,10 +104,8 @@ module.exports.updatePost = async (req, res) => {
 module.exports.deletePost = async (req, res) => {
     const { id } = req.params;
     const post = await igPost.findById(id);
-    const jobs = await agenda.jobs({attrs: {data: {} }})
+    const jobs = await agenda.jobs({job: { attrs: { data: [{}] }})
     console.log('data array ' + jobs);
-    const jobs2 = await agenda.jobs({data: [{postID: post._id}] })
-    console.log('data array ' + jobs2);
     // const count = await agenda.cancel({ attrs: { data: { postID: post._id }}});
     // console.log(count);
     
