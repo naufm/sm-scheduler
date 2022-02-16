425062448a95fe46a9f0c7ed35ca624c231b2fcc
Naufal Miswadi <naufal.miswadi@gmail.com>
2022-02-16 16:26:20 +0800

    fixed syntax error

--- agenda/agenda.js
+++ agenda/agenda.js
@@ -15,9 +15,7 @@ agenda.define('schedule instagram image post', async job => {
     const { data } = job.attrs;
     const user = await User.find({ _id: data.userID });
     const caption = data.caption.replaceAll('#', '%23');
-    const igContainer = `https://graph.facebook.com/${user[0].instaID}/media
-    ?image_url=${data.mediaPath}
-    &caption=${caption}`;
+    const igContainer = `https://graph.facebook.com/${user[0].instaID}/media?image_url=${data.mediaPath}&caption=${caption}`;
     const containerID = await fetch(igContainer, { method: 'POST' });
     const igPublish = `graph.facebook.com/${user[0].instaID}/media_publish?creation_id=${containerID}`;
     await fetch(igPublish, { method: 'POST' });
@@ -28,10 +26,7 @@ agenda.define('schedule instagram video post', async job => {
     const { data } = job.attrs;
     const user = await User.find({ _id: data.userID });
     const caption = data.caption.replaceAll('#', '%23');
-    const igContainer = `https://graph.facebook.com/${user[0].instaID}/media
-    ?media_type=VIDEO
-    &video_url=${data.mediaPath}
-    &caption=${caption}`;
+    const igContainer = `https://graph.facebook.com/${user[0].instaID}/media?media_type=VIDEO&video_url=${data.mediaPath}&caption=${caption}`;
     const containerID = await fetch(igContainer, { method: 'POST' });
     const igPublish = `graph.facebook.com/${user[0].instaID}/media_publish?creation_id=${containerID}`;
     await fetch(igPublish, { method: 'POST' });
